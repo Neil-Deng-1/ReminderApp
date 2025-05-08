c210a5711b68d21d43a4d382a2a3f4b8ad846a4b
Aditya Chattopadhyay <adichatt@terpmail.umd.edu>
2025-05-08 17:12:18 -0400

    Added Firebase stuff

--- app/src/main/java/com/example/reminderapp/ReminderActivity.kt
+++ app/src/main/java/com/example/reminderapp/ReminderActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ReminderActivity : AppCompatActivity() {
     private lateinit var addReminderBtn : Button
@@ -18,5 +20,12 @@ class ReminderActivity : AppCompatActivity() {
             val intent = Intent(this, AddActivity::class.java)
             startActivity(intent)
         }
+
+        var firebase : FirebaseDatabase = FirebaseDatabase.getInstance()
+
+        // Need a reference to write
+        var reference : DatabaseReference = firebase.getReference("country")
+        // Now we can associate the value 'USA' with the key 'country'
+        reference.setValue("USA")
     }
 }
