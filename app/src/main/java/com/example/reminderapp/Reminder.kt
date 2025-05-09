53fc8926e7b2fc30b5ee2a22c59fd16f8431ac41
Aditya Chattopadhyay <adichatt@terpmail.umd.edu>
2025-05-09 12:51:30 -0400

    Turned Reminder from a Dataclass to a Normal Class

--- app/src/main/java/com/example/reminderapp/Reminder.kt
+++ app/src/main/java/com/example/reminderapp/Reminder.kt
@@ -1,8 +1,31 @@
 package com.example.reminderapp
 
-data class Reminder(
-    val title: String,
-    val date: String,
-    val time: String,
-    val description: String
-)
+class Reminder {
+    private lateinit var title: String
+    private lateinit var date: String
+    private lateinit var time: String
+    private lateinit var description: String
+
+    constructor (title : String, date : String, time : String, description : String) {
+        this.title = title
+        this.date = date
+        this.time = time
+        this.description = description
+    }
+
+    fun getTitle() : String {
+        return title;
+    }
+
+    fun getDate() : String {
+        return date;
+    }
+
+    fun getTime() : String {
+        return time;
+    }
+
+    fun getDescription() : String {
+        return description;
+    }
+}
